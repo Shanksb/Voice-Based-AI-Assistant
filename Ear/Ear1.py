f1516aa6751e6319ede98257052e8b42b4e9660d
ShanksB <shashankbhoi1724@gmail.com>
2026-07-27 01:01:46 +0530

    ear done

--- Ear/Ear1.py
+++ Ear/Ear1.py
@@ -9,13 +9,10 @@ def listen_and_transcribe():
         device="cpu",
         compute_type="float32",
         cpu_threads=8,
-        language="en",
-        temperature=0.0,
-        initial_prompt="Hello, my name is Shashank. I am speaking English with a slight Indian accent."
         )
     
     sample_rate = 16000
-    duration = 5
+    duration = 8
     
     print("Listening... (Press Ctrl+C to stop)")
     try:
@@ -25,7 +22,11 @@ def listen_and_transcribe():
             
             audio_data = np.squeeze(audio_chunk)
             
-            segments, _ = model.transcribe(audio_data, beam_size=5, vad_filter=True)
+            segments, _ = model.transcribe(audio_data, beam_size=5, vad_filter=True,
+                                           language="en",
+                                            temperature=0.0,
+                                            initial_prompt="Hello, my name is Shashank. I am speaking English with a slight Indian accent."
+                                            )
         
             for segment in segments:
                 text = segment.text.strip()
